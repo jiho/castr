fe46aaafd51a214f7f612969cf7ed15a2da081b3
Jean-Olivier Irisson <irisson@normalesup.org>
2018-03-15 23:18:37 +0100

    Add utility functions

---
+++ R/check_input.R
@@ -0,0 +1,27 @@
+#' Check input data
+#'
+#' Check that the input data is usable as a CTD cast (not fully missing and of compatible length). Used by other functions.
+#'
+#' @param x vector of the variable of interest.
+#' @param depth vector of depths at which `x` is measured.
+#'
+#' @return TRUE when the data is ok, FALSE when it is fully missing, FALSE and throws an error when `depth` is not the same length as `x`.
+#' @examples
+#' check_input(c(22.1, 23.4, 23.2), c(1, 2, 3))
+#' check_input(c(NA, NA, NA), c(1, 2, 3))
+#' \dontrun{
+#' check_input(c(22.1, 23.4, 23.2), c(1, 2))}
+check_input <- function(x, depth) {
+  ok <- TRUE
+  # check the input
+  if (all(is.na(x))) {
+    ok <- FALSE
+  }
+  if (!is.null(depth)) {
+    if (length(depth) != length(x)) {
+      ok <- FALSE
+      stop("The vector of data (n=", length(x), ") should be as long as the vector of depths (n=", length(depth), ")")
+    }
+  }
+  return(ok)
+}
---
+++ R/get_depth.R
@@ -0,0 +1,24 @@
+#' Get the depth at index i
+#'
+#' Simply extracts element `i` of `depth` but includes failsafes for missing depth or missing `i`. Used by other functions.
+#'
+#' @param i element of `depth` to extract.
+#' @inheritParams check_input
+#'
+#' @return If both `i` and `depth` are provided, return `depth[i]`. If `i` is NULL/empty, return NA. If `depth` is NULL return `i`.
+#'
+#' @examples
+#' get_depth(2, c(34, 35, 36))
+#' get_depth(2, NULL)
+#' get_depth(NULL, c(34, 35, 36))
+#' get_depth(c(), c(34, 35, 36))
+get_depth <- function(i, depth) {
+  if (length(i) > 0) {
+    if (!is.null(depth)) {
+      i <- depth[i]
+    }
+  } else {
+    i <- NA
+  }
+  return(i)
+}
